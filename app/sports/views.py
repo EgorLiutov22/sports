0984814c9f7be448fbf9ae67ca3ea479ae5b1dc1
lyutov_e <eliutov22@gmail.com>
2024-11-19 20:56:28 +0500

    add get_context

--- app/sports/views.py
+++ app/sports/views.py
@@ -8,21 +8,15 @@ import time
 
 def index(request):
     template = loader.get_template("sports/index.html")
-    context = {'title': 'Главная страница',
-               'pages': [('football', 'Футбол'),
-                         ('basketball', 'Баскетбол'),
-                         ('hockey', 'Хоккей'),
-                         ('daytime/', 'Время')
-                         ]}
+    context = get_context('Главная страница')
     return HttpResponse(template.render(context, request))
 
 
 def sport(request, sport_name):
     template = loader.get_template("sports/sport.html")
-    context = {'title': sport_name,
-               'text': f'Новости {sport_name}'
-               }
+    context = get_context(sport_name, {'text': f'Новости {sport_name}'})
     return HttpResponse(template.render(context, request))
+
 
 def daytime(request):
     template = loader.get_template("sports/daytime.html")
@@ -34,8 +28,21 @@ def daytime(request):
         text = 'Добрый день'
     else:
         text = 'Добрый вечер'
-    context = {'title': 'Время',
-               'time': time.strftime("%H:%M:%S", time.localtime()),
-               'daytime': text
-               }
+
+    t = {'time': time.strftime("%H:%M:%S", time.localtime()),
+         'daytime': text}
+    context = get_context('Время', t)
     return HttpResponse(template.render(context, request))
+
+
+def get_context(title, d=None):
+    context = {'title': title,
+               'pages': [('football', 'Футбол'),
+                         ('basketball', 'Баскетбол'),
+                         ('hockey', 'Хоккей'),
+                         ('daytime/', 'Время')
+                         ]}
+    if type(d) == "<class 'dict'>":
+        for k in d:
+            context[k] = d[k]
+    return context
